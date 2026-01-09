af3026df61b8a7277d93578eb34e1b6b850d3e21
Eman Torr Garcia Berjame <etgberjame@addunet.ph>
2026-01-09 17:25:47 +0800

    Event Driven Programming commit

---
+++ script.js
@@ -0,0 +1,81 @@
+// console.log("Hello, World!");
+
+// const add = (a, b) => a + b;
+// console.log("2 + 3 =", add(2, 3));
+
+// const multiply = (a, b) => a * b;
+// console.log("4 * 5 =", multiply(4, 5));
+
+// const greet = (name) => `Hello, ${name}!`;
+// console.log(greet("Alice"));
+
+// const factorial = (n) => {
+//     if (n === 0) return 1;
+//     return n * factorial(n - 1);
+// }
+// console.log("5! =", factorial(5));
+
+// const fibonacci = (n) => {
+//     if (n <= 1) return n;
+//     return fibonacci(n - 1) + fibonacci(n - 2);
+// }
+// console.log("Fibonacci(6) =", fibonacci(6));
+
+// const isEven = (n) => n % 2 === 0;
+// console.log("Is 4 even?", isEven(4));
+// console.log("Is 7 even?", isEven(7));
+
+// const reverseString = (str) => str.split('').reverse().join('');
+// console.log("Reverse of 'JavaScript' is", reverseString("JavaScript"));
+
+// const square = (n) => n * n;
+// console.log("Square of 6 is", square(6));
+
+// const cube = (n) => n * n * n;
+// console.log("Cube of 3 is", cube(3));
+
+// const power = (base, exp) => Math.pow(base, exp);
+// console.log("2 to the power of 4 is", power(2, 4));
+
+// const max = (a, b) => (a > b ? a : b);
+// console.log("Max of 10 and 20 is", max(10, 20));
+
+// const min = (a, b) => (a < b ? a : b);
+// console.log("Min of 10 and 20 is", min(10, 20));
+
+// const average = (arr) => arr.reduce((sum, val) => sum + val, 0) / arr.length;
+// console.log("Average of [1, 2, 3, 4, 5] is", average([1, 2, 3, 4, 5]));
+
+// const isPrime = (n) => {
+//     if (n <= 1) return false;
+//     for (let i = 2; i <= Math.sqrt(n); i++) {
+//         if (n % i === 0) return false;
+//     }
+//     return true;
+// }
+// console.log("Is 11 prime?", isPrime(11));
+// console.log("Is 15 prime?", isPrime(15));
+
+// const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+// console.log("Capitalize 'hello':", capitalize("hello"));
+
+// const flattenArray = (arr) => arr.reduce((flat, toFlatten) => flat.concat(Array.isArray(toFlatten) ? flattenArray(toFlatten) : toFlatten), []);
+// console.log("Flatten [1, [2, [3, 4], 5]]:", flattenArray([1, [2, [3, 4], 5]]));
+
+// const removeDuplicates = (arr) => [...new Set(arr)];
+// console.log("Remove duplicates from [1, 2, 2, 3, 4, 4, 5]:", removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+
+// const sortArray = (arr) => arr.slice().sort((a, b) => a - b);
+// console.log("Sort [5, 3, 8, 1, 2]:", sortArray([5, 3, 8, 1, 2]));
+
+// console.log("All operations completed.");
+
+// // End of code
+
+// var x = 0;
+// var y = 1;
+
+// var z = x + y;
+// console.log("x + y =", z);
+
+
